42476f106244118dc46e5dda96df50b409fd0f2f
RobertaKelly <robatucadas@hotmail.com>
2020-11-02 21:14:00 -0300

    test

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
 const cipher = {
-  // ...
+ 
+  //variavel para calcular codigo da letra , tamanho alfabeto .
+  //const desloc = codigoDaletraAsc =65+offset%26+65
 };
+  
+
+//
+//};
 
 export default cipher;
+
+
+//codigoAsc => codigo0a25 => desloc => giro = codigoAsc
+//( codigoDaletra + desloc) %26 tamDoAlfabeto
+//( cidigoDaletra + 7) %26 
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
+function Criptografarmsg(event) { 
+    event.preventDefault()
+    const textcrypto = document.getElementById("textcrypto").value;
+    const cryptoffset = parseInt(document.getElementById ("cryptoffset")).value;
+    const resultcrypto = document.getElementById("resultcrypto");
+ for (let i=0; i<=textcrypto.length;1++) {
+     
+ }
+
+}
+
+function Decriptografar(event) { 
+    event.preventDefault()
+    const textdecrypto = document.getElementById("textdecrypto").value;
+    const cryptoffset = parseInt(document.getElementById ("decryptoffset")).value;
+    const resultdecrypto = document.getElementById("resultdecrypto");
+}
+
+
+//botão para Criptografar. 
+let btn = document.getElementById("submit1")
+btn.addEventListener('click', textcrypto);
+
+
+//botão para Descriptografar 
+let btn = document.getElementById("submit2")
+btn2.addEventListener('click', textdecrypto);
+
+document.getElementById()
+
+
 console.log(cipher);
